4b2daa59d4e5dac141ffca30a451896afbca2948
DSunShine371 <pestic077@gmail.com>
2025-04-29 17:07:58 +0300

    Step 1 Setup fix sonar 3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,6 @@ package hexlet.code;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("New App project");
+        System.out.println("New App Project");
     }
 }
